dd5cd0e4c3a4900a22705e3f62d3153c40fb4b3a
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-03 23:54:06 +0530

    Update today

--- dev_react_app/src/Components/Main.js
+++ dev_react_app/src/Components/Main.js
@@ -1,8 +1,10 @@
 function Main(props){
+    const name = "Janith Nanayakkara"
+    const position = "Full Stack Developer"
     return(<div>This is Main Components
         <ul>
-            <li>Java</li>
-            <li>React</li>
+            <li>{name}</li>
+            <li>{position}</li>
             <li>CSS</li>
             <li>JavaScript</li>
         </ul>
