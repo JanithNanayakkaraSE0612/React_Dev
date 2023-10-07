21a0a3584e26e857482ae60082c1ad11998cd68c
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-10-08 03:29:40 +0530

    Change data id

--- dev_react_app/src/App.js
+++ dev_react_app/src/App.js
@@ -3,25 +3,29 @@ import './App.css';
 import Main from './Components/Main';
 
 const myData = [
-  {
+  { 
+    Id: 1234,
     name :'Nimal',
     city :'Matara',
     position :'app-developer'
 
   },
   {
+    id : 1235,
     name :'Nimal',
     city :'Matara',
     position :'app-developer'
 
   },
   {
+    id : 1236,
     name :'Nimal',
     city :'Matara',
     position :'app-developer'
 
   },
   {
+    id : 1237,
     name :'Nimal',
     city :'Matara',
     position :'app-developer'
