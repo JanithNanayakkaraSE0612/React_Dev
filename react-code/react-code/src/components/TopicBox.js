45f1c852d81e672dfcca0369d69f11c2d0ad6c15
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-08 01:58:58 +0530

    Creating Dynamic Components

--- react-code/react-code/src/components/TopicBox.js
+++ react-code/react-code/src/components/TopicBox.js
@@ -2,10 +2,13 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox() {
+    const topic = "My Favarite Food Is Rice"
+    const item = ".add kottu"
   return (
     <div>
        <div className="topicbox">
-            <span className='text'>This is the Topic</span>
+            <span className='text'>This is the Topic
+            <br></br>{topic}{item}{alert("hello")}</span>
        </div>
     </div>
   )
