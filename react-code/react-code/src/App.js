1ab53f1ec4f84e3dc2802fc2e7a7e8c9378da158
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-09 03:32:22 +0530

    UseState Complete

--- react-code/react-code/src/App.js
+++ react-code/react-code/src/App.js
@@ -2,18 +2,29 @@ import './App.css';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  let count = 0;
+  const [count,setCount] = useState(0);
+  //let count = 0;
+
+  const increment = () =>{
+    //count +=1;
+    setCount(count+1);
+  }
+  const decrement = () =>{
+   // count -=1;  
+   setCount(count-1);
+  }
   return (
     <>
     <span className='title'>
       My Counter
     </span>
     <p className='subtitle'>The Count is {count}</p>
-    <button className='button'>-</button>
-    <button className='button'>+</button>
+    <button onClick={decrement} className='button'>-</button>
+    <button onClick={increment} className='button'>+</button>
     </>
     // <Router>
     //   <div>
