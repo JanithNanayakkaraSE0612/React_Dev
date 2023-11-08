21e163f8c8fb404840dd123f6055d8fde0a710b1
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-08 14:49:52 +0530

    Props Handling

--- react-code/react-code/src/components/TopicBox.js
+++ react-code/react-code/src/components/TopicBox.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './topicbox.css'
 
-export default function TopicBox() {
+export default function TopicBox(props) {
     const topic = "My Favarite Food Is Rice"
     const item = ".add kottu"
   return (
     <div>
        <div className="topicbox">
-            <span className='text'>This is the Topic
+            <span className='text'>This is the Topic{props.food} this is {props.price}
             <br></br>{topic}{item}{alert("hello")}</span>
+            {props.children}
        </div>
     </div>
   )
--- react-code/react-code/src/pages/Home.js
+++ react-code/react-code/src/pages/Home.js
@@ -6,8 +6,14 @@ export default function home() {
   return (
     <div>
       <h1>This is Home Page</h1>
-      <TopicBox/>
-      <Content/>
+      <TopicBox food="kottu" price="Rs 250">
+        <span>This is the description</span>
+      </TopicBox>
+      <TopicBox food="Rice" price="Rs 550">
+        <button>This is a Button</button>
+      </TopicBox>
+      <TopicBox food="Hoppers" price="Rs 450"/>
+       <Content/>
     </div>
   )
 }
