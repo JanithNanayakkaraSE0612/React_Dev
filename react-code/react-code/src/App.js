a7fad97dd53affc526768f9d10295f90d0dfe066
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-10 16:57:39 +0530

    useEffect Hook part 1

--- react-code/react-code/src/App.js
+++ react-code/react-code/src/App.js
@@ -9,57 +9,57 @@ function buttonClick(){
   console.log("Button is Clicked");
 }
 function App() {
-  // const [count,setCount] = useState(0);
-  // //let count = 0;
+  const [count,setCount] = useState(0);
+  //let count = 0;
 
-  // const increment = () =>{
-  //   //count +=1;
-  //   setCount(count+1);
-  // }
-  // const decrement = () =>{
-  //  // count -=1;  
-  //  setCount(count-1);
-  // }
+  const increment = () =>{
+    //count +=1;
+    setCount(count+1);
+  }
+  const decrement = () =>{
+   // count -=1;  
+   setCount(count-1);
+  }
   return (
-    <div className='according'>
-    <h1>React Event Handling</h1>
-    <br></br><hr></hr>
-    <button onClick={buttonClick}>Click Here</button>
-    {/* <Accordion defaultActiveKey="0">
-    <Accordion.Item eventKey="0">
-      <Accordion.Header>Accordion Item #1</Accordion.Header>
-      <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.
-      </Accordion.Body>
-    </Accordion.Item>
-    <Accordion.Item eventKey="1">
-      <Accordion.Header>Accordion Item #2</Accordion.Header>
-      <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.
-      </Accordion.Body>
-    </Accordion.Item>
-  </Accordion> */}
-  </div>
-    // <>
-    // <span className='title'>
-    //   My Counter
-    // </span>
-    // <p className='subtitle'>The Count is {count}</p>
-    // <button onClick={decrement} className='button'>-</button>
-    // <button onClick={increment} className='button'>+</button>
-    // </>
+  //   // <div className='according'>
+  //   // <h1>React Event Handling</h1>
+  //   // <br></br><hr></hr>
+  //   // <button onClick={buttonClick}>Click Here</button>
+  //   {/* <Accordion defaultActiveKey="0">
+  //   <Accordion.Item eventKey="0">
+  //     <Accordion.Header>Accordion Item #1</Accordion.Header>
+  //     <Accordion.Body>
+  //       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  //       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+  //       minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+  //       aliquip ex ea commodo consequat. Duis aute irure dolor in
+  //       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+  //       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+  //       culpa qui officia deserunt mollit anim id est laborum.
+  //     </Accordion.Body>
+  //   </Accordion.Item>
+  //   <Accordion.Item eventKey="1">
+  //     <Accordion.Header>Accordion Item #2</Accordion.Header>
+  //     <Accordion.Body>
+  //       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  //       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+  //       minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+  //       aliquip ex ea commodo consequat. Duis aute irure dolor in
+  //       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+  //       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+  //       culpa qui officia deserunt mollit anim id est laborum.
+  //     </Accordion.Body>
+  //   </Accordion.Item>
+  // </Accordion> */
+  // /* </div> */}
+    <>
+    <span className='title'>
+      My Counter
+    </span>
+    <p className='subtitle'>The Count is {count}</p>
+    <button onClick={decrement} className='button'>-</button>
+    <button onClick={increment} className='button'>+</button>
+    </>
     // <Router>
     //   <div>
     //     <Routes>
