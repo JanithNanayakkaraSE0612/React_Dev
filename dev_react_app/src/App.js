76f08ba64044754b483b7418d204a219ab38b95e
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-24 23:48:52 +0530

    re update

--- dev_react_app/src/App.js
+++ dev_react_app/src/App.js
@@ -5,6 +5,22 @@ import Main from './Components/Main';
 import { myData } from './Data/MyData';
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
     <>
     <div>Janith Induwara Nanayakakra
       <h1>Janith</h1>
