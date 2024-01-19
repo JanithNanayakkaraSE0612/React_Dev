10b677b649c4db814191bd5fa11a053886e696b6
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-20 02:04:45 +0530

    Using Navigate Function

--- test_react_app/src/App.js
+++ test_react_app/src/App.js
@@ -1,11 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
         <header className="App-header">
             <h1>Welcome to Janith Vlogs </h1>
-            <button className="user-button">Users</button>
+            <button className="user-button" onClick={() =>navigate('/users')}>Users</button>
         </header>
     </div>
   );
