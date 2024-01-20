04f1a7a2c4e0170e009c345a7f263fabec39d9a2
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-21 01:47:01 +0530

    Connected To MongoDB

--- test_react_app_back_end/server.js
+++ test_react_app_back_end/server.js
@@ -1,6 +1,24 @@
-const app = require('./app');
 const port = 3001;
 const host = '127.0.0.1';
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://School:janith0202@cluster0.7ay2le9.mongodb.net/?retryWrites=true&w=majority';
+
+const connect = async () =>{
+    try{
+        await mongoose.connect(uri);
+        console.log('Connected to MongoDB');
+    }catch(error){
+        console.log("Not Connected",error); 
+    }
+};
+connect();
+
 const server = app.listen(port,host, ()=>{
     console.log(`Node Server is listening to ${server.address().port}`)
 })
