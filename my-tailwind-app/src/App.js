9d87080dd0702303466d7d2143b9a448d912f410
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-05 21:52:29 +0530

    Tailwind css react testing

---
+++ my-tailwind-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <section className='bg-yellow-200 w-screen h-screen flex-row  '>
+          <div className='bg-red-400 w-36 h-20 mr-16 flex-1'></div>
+          <div className='bg-red-400 w-36 h-20 mb-20'></div>
+          <div className='bg-red-400 w-36 h-20 mr-20'></div>
+
+          <div>Janith</div>
+        </section>
+    </div>
+  );
+}
+
+export default App;
