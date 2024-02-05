69aeabb73e7ee2b1d13ab8779126082c06a67123
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-05 19:31:32 +0530

    Remove App.js

--- dev_react_app/src/App.js
+++ dev_react_app/src/App.js
@@ -1,44 +1,13 @@
 // import logo from './logo.svg';
-import './App.css';
+import '';
 import Main from './Components/Main';
 
 import { myData } from './Data/MyData';
+import Home from './pages/Home';
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <>
-    <div>Janith Induwara Nanayakakra
-      <h1>Janith</h1>
-    </div>
-    <p>I'm Janith Nanayakakra Senior Software Engineer</p>
-    {/* <Main  name="Navindu" city="Galle" position="Web-developer">
-      <p>Software Developer</p>
-      <p>
-        I'm Children
-      </p>
-    </Main>
-    <Main name="kavindu" city="Tangalla" position="app-developer"/>
-    <Main name="Gayani" city= "Elpitiya" position="UI / UX Developer"/> */
-    myData.map(({name,city,position,id})=>{
-      // assign key value
-      return <Main key={id} name={name} city={city} position={position}/>
-    })
-    }
+    <Home/>
     </>
   );
 }
