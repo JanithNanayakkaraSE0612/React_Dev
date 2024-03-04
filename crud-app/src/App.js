f7871065c8c3cfa99d91e92f0f902a3dc5cacd66
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-03-04 19:09:38 +0530

    update

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,1 +1,13 @@
 import React ,{Component} from 'react';
+import 'boostrap/dist/css/boostrap.min.css';
+
+class App extends Component{
+    render(){
+        return(
+            <div className='container'>
+                <h2>React CRUD by Janith</h2>
+            </div>
+        );
+    }
+}
+export default App;
