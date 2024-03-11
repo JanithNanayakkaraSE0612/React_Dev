9d5009e2c4ef41fd60d4c2155a02a7fe2c6a6d49
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-03-12 00:33:25 +0530

    Install server side dependency

--- my-crud-app/src/App.js
+++ my-crud-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="">
-        <h1 className='text-red-700'>Janith</h1>
+        <h1 className='text-red-700 text-lg text-center'>Janith</h1>
     </div>
   );
 }
