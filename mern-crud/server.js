9206f1f1017f6f324bf5a59c49b406d078d7b9d6
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-03-12 21:38:48 +0530

    Connect Mongoose

--- mern-crud/server.js
+++ mern-crud/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const port = 8000;
+const PORT = 8000;
+const DB_URL ='mongodb+srv://crud:crudJanith@crud.1swk79w.mongodb.net/mernCrud?retryWrites=true&w=majority&appName=crud';
 
-app.listen(port,()=>{
-    console.log(`app is running on ${port}`);
+mongoose.connect(DB_URL).then(()=>{
+    console.log('DB Connected');
+}).catch((err)=>{
+    console.log('DB Connected Error',err);
+})
+
+app.listen(PORT,()=>{
+    console.log(`app is running on ${PORT}`);
 });
