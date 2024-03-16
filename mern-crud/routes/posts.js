03d4e454d46b8b8b6bb1143d738f60b62a568705
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-03-16 14:30:02 +0530

    Update and Get Method

--- mern-crud/routes/posts.js
+++ mern-crud/routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Posts = require('../models/posts');
+const posts = require('../models/posts');
 
 const router = express.Router();
 
@@ -19,6 +20,38 @@ router.post('/post/save',(req,res)=>{
             succuess:"Posts saved successfully"
         });
     });
+});
+
+// get posts  
+
+router.get('/post',(req,res)=>{
+    posts.find().exec((err,posts)=>{
+        if(err){
+            return res.status(400).json({
+                Error.err
+            });
+        }
+        return res.status(200).json({
+            succuess:true,
+            existingPosts:posts
+        });
+    });
+});
+
+// update post
+
+router.put('/post/update/:id',(req,res)=>{
+    posts.findByIdAndUpdate(
+        req.params.id,{
+            $set:req.body
+        },
+        (err,post)=>{
+            if(err){
+            return res.status(400).json({Error:err});
+        }
+        return res.status(200).json({succuess:'Update SuccessFully'});
+    }
+    )
 });
 
 module.exports = router;
